47a956b2648267f15f9c49edc5f84c737768235b
M Durrant <mdurrant@DN51t4qb.SUNet>
2017-03-21 19:29:40 -0600

    update

--- setup.py
+++ setup.py
@@ -28,11 +28,11 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/pypa/sampleproject',
+    url='https://github.com/durrantmm/durratools',
 
     # Author details
-    author='The Python Packaging Authority',
-    author_email='pypa-dev@googlegroups.com',
+    author='Matt Durrant',
+    author_email='mdurrant@stanford.edu',
 
     # Choose your license
     license='MIT',
@@ -46,7 +46,7 @@ setup(
         'Development Status :: 3 - Alpha',
 
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
+        'Intended Audience :: d',
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
@@ -54,16 +54,11 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
 
     # What does your project relate to?
-    keywords='sample setuptools development',
+    keywords='biology genomics bioinformatics',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
@@ -106,7 +101,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'sample=sample:main',
+            'durratools=durratools:main',
         ],
     },
 )
