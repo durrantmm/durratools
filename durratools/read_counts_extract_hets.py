92992287b6bdaa6b5bd170845b8380e7a9404c55
M Durrant <mdurrant@Ms-MacBook-Pro.local>
2017-03-31 08:33:16 -0600

    update

--- durratools/read_counts_extract_hets.py
+++ durratools/read_counts_extract_hets.py
@@ -1,6 +1,6 @@
 import sys
 
-def extract_hets(input_read_counts_stream, input_vcf_stream, out_stream, vcf_sample_name):
+def extract_hets(input_read_counts_stream, input_vcf_stream, vcf_sample_name, out_stream):
     hets = set()
     for rec in input_vcf_stream:
         gt = rec.genotype(vcf_sample_name)
