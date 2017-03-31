9ed10db90161e4dbbe3b054a8545498fbe5e9409
M Durrant <mdurrant@Ms-MacBook-Pro.local>
2017-03-31 09:09:25 -0600

    update

--- durratools/read_counts_extract_hets.py
+++ durratools/read_counts_extract_hets.py
@@ -16,6 +16,6 @@ def extract_hets(input_read_counts_stream, input_vcf_stream, vcf_sample_name, ou
     for line in input_read_counts_stream:
         line = line.strip().split()
         chrom = line[0]
-        pos = line[1]
+        pos = int(line[1])
         if (chrom, pos) in hets:
             out_stream.write('\t'.join(line)+'\n')
