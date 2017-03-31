4cf8bb7803af4e10d044a82f2255158df310c1f3
M Durrant <mdurrant@Ms-MacBook-Pro.local>
2017-03-31 08:44:13 -0600

    update

--- durratools/read_counts_extract_hets.py
+++ durratools/read_counts_extract_hets.py
@@ -3,11 +3,13 @@ import sys
 def extract_hets(input_read_counts_stream, input_vcf_stream, vcf_sample_name, out_stream):
 
     hets = set()
+    counter = 1
     for rec in input_vcf_stream:
         gt = rec.genotype(vcf_sample_name)['GT']
         if gt in ['0/1', '1/0', '0|1', '1/0']:
             hets.add((rec.CHROM, rec.POS))
-            print(len(hets))
+            print(counter, len(hets))
+        counter += 1
 
     header = input_read_counts_stream.readline().strip().split()
     for line in input_read_counts_stream:
